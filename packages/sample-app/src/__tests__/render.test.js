8168c82b08d1312717d6a510a9d32e648381f2b9
Luis <takahashihideki408@gmail.com>
2020-04-12 13:52:57 -0300

    Added custom render  wrapper

---
+++ packages/sample-app/src/__tests__/render.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { getQueriesForElement } from '@testing-library/dom';
+
+import LogoImage from  '../logo.svg';
+import App from '../App';
+
+const render = (Ui) => {
+  const container  = document.createElement('div');
+
+  ReactDOM.render(Ui, container);
+
+  const queries = getQueriesForElement(container);
+
+  return {
+    container,
+    ...queries
+  }
+}
+
+test('renders learn react link with logo', () => {
+  const { getByAltText, getByText } = render(<App />);
+
+  const image = getByAltText('logo');
+  expect(image).toHaveAttribute('src', LogoImage);
+
+  const link = getByText('Learn React');
+  expect(link).toHaveAttribute('href');
+});
