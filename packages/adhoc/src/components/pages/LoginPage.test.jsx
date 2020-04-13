b1f31bd2b095351ffe93d08b8c047689410f1bb5
andre-matulionis-ifood <andre.matulionis@ifood.com.br>
2020-04-13 16:11:49 -0300

    removes redux wrapper

--- packages/adhoc/src/components/pages/LoginPage.test.jsx
+++ packages/adhoc/src/components/pages/LoginPage.test.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from '../../reducers/reducer';
 import LoginPage from './LoginPage';
 
-let wrapper;
-beforeEach(() => {
-  wrapper = ({ children }) => (
-    <Provider store={createStore(reducer)}>{children}</Provider>
-  );
-});
-
 test('Logging in', () => {
-  render(<LoginPage />, { wrapper });
+  render(<LoginPage />);
 
   const userInput = screen.getByLabelText(/user/i);
   fireEvent.change(userInput, { target: { value: 'master' } });
@@ -25,6 +15,5 @@ test('Logging in', () => {
   fireEvent.click(submitButton);
 
   const successMessage = screen.getByText(/olá/i);
-
   expect(successMessage).not.toBeNull();
 });
