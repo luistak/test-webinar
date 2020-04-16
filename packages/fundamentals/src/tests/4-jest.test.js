fd16f98de83d835017adfb1a9cb3e49c031e49f0
andre-matulionis-ifood <andre.matulionis@ifood.com.br>
2020-04-16 20:16:28 -0300

    updates on fundamentals

--- packages/fundamentals/src/tests/4-jest.test.js
+++ packages/fundamentals/src/tests/4-jest.test.js
@@ -1,17 +1,15 @@
 const { sum, subtract } = require('../math');
 
-let result, expected;
-
 test('should sum numbers', () => {
-  result = sum(10, 5);
-  expected = 15;
+  const result = sum(10, 5);
+  const expected = 15;
 
   expect(result).toBe(expected);
 });
 
 test('should subtract numbers', () => {
-  result = subtract(10, 5);
-  expected = 5;
+  const result = subtract(10, 5);
+  const expected = 5;
 
   expect(result).toBe(expected);
 });
