72d2452285365017d57a0a6eae22a65195700876
Luis <takahashihideki408@gmail.com>
2020-11-11 01:52:17 -0300

    Added initial integration exercise

--- packages/integrations/src/App.test.js
+++ packages/integrations/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('complete order checkout', () => {
+  // TODO: Assert initial page
+
+  // TODO: Find and navigate do first item details
+  
+  // TODO: Add item to cart
+
+  // TODO: Go to Cart
+
+  // TODO: Checkout
+
+  // TODO: Assert redirect to initial page
 });
--- packages/integrations/src/mocks.js
+++ packages/integrations/src/mocks.js
@@ -11,6 +11,6 @@ export const mockedItems = [
     "price": 699,
     "name": "Teclado Mx Keys",
     "about": "https://www.logitech.com/pt-br/product/mx-keys-wireless-keyboard",
-    "image": "https://www.logitechstore.com.br/media/catalog/product/cache/1/image/500x391/9df78eab33525d08d6e5fb8d27136e95/m/x/mx-keys_1.png"
+    "image": "https://resource.logitech.com/content/dam/logitech/en/products/keyboards/mx-keys/mx-keys-full-screen-feature-1a-image-desktop.png"
   }
 ]
